9f12f58c825a19221cdce6b6648653def988d495
Mr.Xiang <406862257@qq.com>
2024-07-01 15:49:32 +0800

    组件开发

---
+++ src/components/Menus.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>菜单</h2>
+  </div>
+</template>
+<script>
+export default{
+
+}
+</script>
+<style scoped>
+h2{
+  color: red;
+}
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,26 +1,33 @@
 <template>
   <div>
-    <h2>首页</h2>
+    <!-- 1.header：logo、搜索、头像 -->
+    <!-- 3.使用组件 -->
+    <Header></Header>
 
-    <RouterLink to="/play">去播放页</RouterLink>
-    <hr>
-    <button @click="goPlay()">去播放页</button>
-    <hr>
-    <button @click="$router.push('/play')">去播放页</button>
+    <!-- 2.菜单 -->
+    <Menus></Menus>
+
+    <!-- 3.轮播图、热门视频 -->
+
+    <!-- 4.其它视频的列表 -->
   </div>
 </template>
 
 <script>
 import { RouterLink} from 'vue-router'
+// 1.导入组件
+import Header from '../components/Header.vue'
+import Menus from '../components/Menus.vue'
 
 export default{
   // 写事件函数，例如点击、双击、change事件等等
   methods:{
-    goPlay(){
-      console.log("按钮被点击了")
-      // 通过路由跳转页面
-      this.$router.push("/play")
-    }
+    
+  },
+  components:{
+    // 2.注册组件
+    Header,
+    Menus
   }
 }
 </script>
