f8054359639e9ecfe1f20463e9a9c12e845a6386
Mr.Xiang <406862257@qq.com>
2024-07-01 17:08:33 +0800

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,46 +1,117 @@
 <template>
-  <div>
-    <h2>头部</h2>
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
+  <div style="background-color: black;">
+    <!-- 菜单、输入框、头像 -->
+    <!-- 行 :gutter="20" 指定列之间的间距  单位px -->
+    <el-row :gutter="20">
+      <!-- 列 span：指定当前el-col占用多少列-->
+      <el-col :span="10">
+        <ul class="menu">
+          <li><a href="#">首页</a></li>
+          <li><a href="#">番剧</a></li>
+          <li><a href="#">直播</a></li>
+          <li><a href="#">游戏中心</a></li>
+          <li><a href="#">会员购</a></li>
+          <li><a href="#">漫画</a></li>
+          <li><a href="#">赛事</a></li>
+          <li><a href="#">欧洲杯</a></li>
+          <li><a href="#">周年庆</a></li>
+        </ul>
+      </el-col>
 
-    <el-button plain>Plain</el-button>
-    <el-button type="primary" plain>Primary</el-button>
-    <el-button type="success" plain>Success</el-button>
-    <el-button type="info" plain>Info</el-button>
-    <el-button type="warning" plain>Warning</el-button>
-    <el-button type="danger" plain>Danger</el-button>
+      <!-- 搜索输入框 -->
+      <el-col :span="6">
+        <!-- 输入框 -->
+        <el-input placeholder="请输入搜索内容"></el-input>
+      </el-col>
 
-    <el-button round>Round</el-button>
-    <el-button type="primary" round>Primary</el-button>
-    <el-button type="success" round>Success</el-button>
-    <el-button type="info" round>Info</el-button>
-    <el-button type="warning" round>Warning</el-button>
-    <el-button type="danger" round>Danger</el-button> 
+      <!-- 头像、个人、投稿 -->
+      <el-col :span="8">
+        <ul class="menu">
+          <li style="margin-left: 10px;margin-right: 10px;">
+            <el-icon color="white">
+              <Avatar />
+            </el-icon>
+            <div style="color: white;">大会员</div>
+          </li>
+          <li style="margin-left: 10px;margin-right: 10px;">
+            <el-icon color="white">
+              <Avatar />
+            </el-icon>
+            <div style="color: white;">消息</div>
+          </li>
+          <li style="margin-left: 10px;margin-right: 10px;">
+            <el-icon color="white">
+              <Avatar />
+            </el-icon>
+            <div style="color: white;">动态</div>
+          </li>
+          <li style="margin-left: 10px;margin-right: 10px;">
+            <el-icon color="white">
+              <Avatar />
+            </el-icon>
+            <div style="color: white;">历史</div>
+          </li>
+          <li style="margin-left: 10px;margin-right: 10px;">
+            <el-icon color="white">
+              <Avatar />
+            </el-icon>
+            <div style="color: white;">创作中心</div>
+          </li>
+          <li style="margin-left: 10px;margin-right: 10px;">
+            <div style="color: white;">
+              <el-button type="danger"
+                         @click="upload">
+                <el-icon>
+                  <UploadFilled />
+                </el-icon>投稿
+              </el-button>
+            </div>
+          </li>
+        </ul>
+      </el-col>
+    </el-row>
 
-    <el-button circle >
-      <el-icon>
-        <Edit/>
-      </el-icon>
-    </el-button>
-
-
-    <el-button type="primary" :icon="Edit" circle />
-    <el-button type="success" :icon="Check" circle />
-    <el-button type="info" :icon="Message" circle />
-    <el-button type="warning" :icon="Star" circle />
-    <el-button type="danger" :icon="Delete" circle />
+    <!-- logo  :offset="20"  设置偏移量-->
+    <el-row>
+      <el-col :span="4">
+        <img src="@/assets/logo.png"
+             alt=""
+             style="width: 100%; height: auto;">
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
-export default{
-
-}
+export default {}
 </script>
 <style scoped>
+.menu {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+.menu li {
+  float: left;
+}
 
+.menu li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 10px 10px;
+  text-decoration: none;
+}
+
+/* 鼠标悬停时的样式 */
+.menu li a:hover {
+  font-size: 14px; /* 增加字体大小 */
+}
+
+/* 清除浮动 */
+.menu:after {
+  content: '';
+  display: table;
+  clear: both;
+}
 </style>
