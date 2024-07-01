c571de85cebed8353a26fba5fd4b026cdf19a15d
Mr.Xiang <406862257@qq.com>
2024-07-01 15:34:28 +0800

    路由

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
     <h2>首页</h2>
+
+    <RouterLink to="/play">去播放页</RouterLink>
+    <hr>
+    <button @click="goPlay()">去播放页</button>
+    <hr>
+    <button @click="$router.push('/play')">去播放页</button>
   </div>
 </template>
 
 <script>
-export default{
+import { RouterLink} from 'vue-router'
 
+export default{
+  // 写事件函数，例如点击、双击、change事件等等
+  methods:{
+    goPlay(){
+      console.log("按钮被点击了")
+      // 通过路由跳转页面
+      this.$router.push("/play")
+    }
+  }
 }
 </script>
 
--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <h2>视频播放页</h2>
+    <RouterLink to="/">去首页</RouterLink>
   </div>
 </template>
 
 <script>
+import { RouterLink} from 'vue-router'
 export default{
 
 }
