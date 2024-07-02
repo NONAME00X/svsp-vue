4e887a6aa050a86577c20c894dd6a8e338a9c892
Mr.Xiang <406862257@qq.com>
2024-07-02 17:31:19 +0800

    个人中心

--- src/views/up/UpPersonal.vue
+++ src/views/up/UpPersonal.vue
@@ -1,11 +1,79 @@
 <template>
   <div>
-    <h2>up主个人页面</h2>
+    <el-container style="">
+      <el-header style="height: 140px;background-color:black;">
+        <Header></Header>
+      </el-header>
+      <el-container style="">
+        <el-aside width="200px" style="height: 600px;">
+          <el-scrollbar>
+            <el-menu :default-openeds="['1', '3']">
+              <el-sub-menu index="1">
+                <template #title>
+                  <el-icon><message /></el-icon>Navigator One
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="1-1">Option 1</el-menu-item>
+                  <el-menu-item index="1-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="1-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="1-4">
+                  <template #title>Option4</template>
+                  <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-sub-menu index="2">
+                <template #title>
+                  <el-icon><icon-menu /></el-icon>Navigator Two
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="2-1">Option 1</el-menu-item>
+                  <el-menu-item index="2-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="2-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="2-4">
+                  <template #title>Option 4</template>
+                  <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-sub-menu index="3">
+                <template #title>
+                  <el-icon><setting /></el-icon>Navigator Three
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="3-1">Option 1</el-menu-item>
+                  <el-menu-item index="3-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="3-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="3-4">
+                  <template #title>Option 4</template>
+                  <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+            </el-menu>
+          </el-scrollbar>
+
+        </el-aside>
+        <el-main style="">Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script>
+import Header from '../../components/Header.vue'
 export default{
-
+  components:{
+    Header
+  }
 }
 </script>
 <style scoped>
