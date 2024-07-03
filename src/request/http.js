3de0867ceb378d69cc15303add7fe3bff0daff5e
Mr.Xiang <406862257@qq.com>
2024-07-03 10:47:34 +0800

    请求拦截器

--- src/request/http.js
+++ src/request/http.js
@@ -5,5 +5,18 @@ const http = axios.create({
   baseURL: '/api/', // 设置基础 URL  http://localhost:8080/api/user/login
   // 其他配置...
 });
+// 请求拦截器：会在axios所有请求发送出去之前执行一段特点的代码，例如将token放到请求头中
+http.interceptors.request.use(config =>{
+  // 从本地获取token
+  let token = window.localStorage.getItem("token")
+
+  if(token){
+    config.headers.Authorization = token
+  }
+
+  // 可以在代码中做逻辑判断，是否要放行这个请求
+  // 放行所有请求
+  return config
+})
 
 export default http
--- src/views/up/VideoManage.vue
+++ src/views/up/VideoManage.vue
@@ -1,11 +1,43 @@
 <template>
   <div>
-    <h2>视频管理页面</h2>
+    <!-- 搜索、添加 -->
+    <el-row>
+      <el-col :span="8">
+        <el-input placeholder="视频标题" style="width: 80%;"></el-input>
+        <el-button type="success">搜索</el-button>
+      </el-col>
+      <el-col :span="1" :offset="15">
+        <el-button type="primary">添加</el-button>
+      </el-col>
+    </el-row>
+
+    <!-- 表格 -->
+    <el-row>
+
+    </el-row>
+
+    <!-- 分页 -->
+    <el-row></el-row>
   </div>
 </template>
 <script>
 export default{
-
+  data(){
+    return{
+      page: 1, // 页面
+      size: 5, // 每一页多少条数据
+      total: 0, // 总条数
+      videoList: [] // 当前页的视频列表、数据
+    }
+  },
+  mounted(){
+    // 页面加载时去查询出第一页的视频信息
+    // video/findByUid/1/5    video/findByUid?page=1&size=5&account=xxx
+    // `模板字符串`  方便拼接字符串
+    this.$axios.get(`video/findByUid/${this.page}/${this.size}`).then(res =>{
+      console.log(res.data)
+    })
+  }
 }
 </script>
 <style scoped>
