af51c210ee195a69007f0eff24319f2bd263230a
Mr.Xiang <406862257@qq.com>
2024-07-05 15:21:21 +0800

    热门视频

--- src/components/Hot.vue
+++ src/components/Hot.vue
@@ -3,7 +3,7 @@
     <el-row :gutter="10">
       <!-- 轮播图 -->
       <el-col :span="10">
-        <el-carousel height="450px">
+        <el-carousel height="300px">
           <el-carousel-item v-for="item in carousels" :key="item">
             <el-image style="width: 100%; height: 100%" :src="item.cover" fit="contain" />
           </el-carousel-item>
@@ -11,13 +11,18 @@
       </el-col>
 
       <!-- 推荐视频 -->
-      <el-col :span="16">
-
+      <el-col :span="14">
+        <el-row :gutter="15">
+          <el-col :span="8" v-for="abc in videoList" :key="abc">
+            <VideoItem :video="abc"></VideoItem>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
 </template>
 <script>
+import VideoItem from './VideoItem.vue'
 export default{
   // data写当前页面的属性，data在脚手架项目中必须是一个函数，不能是对象：为了在组件复用时不会出现互相干扰
   // data里面必须返回一个对象，对象中就写当前页面的属性
@@ -44,8 +49,20 @@ export default{
           title: "热门推荐4",
           cover: "https://cdjava96.oss-cn-chengdu.aliyuncs.com/c4.png"
         }
-      ]
+      ],
+      videoList: []
     }
+  },
+  mounted(){
+    // 发请求将最新的视频查询出来：审核通过的视频
+    this.$axios.get("video/findNew").then(res => {
+      console.log(res.data)
+
+      this.videoList = res.data.data
+    })
+  },
+  components:{
+    VideoItem
   }
 }
 </script>
---
+++ src/components/VideoItem.vue
@@ -0,0 +1,71 @@
+<template>
+  <div v-if="show"
+       class="item-box">
+    <el-row>
+      <img :src="video.cover"
+           class="cover">
+    </el-row>
+    <el-row class="play-icon">
+      <img src="@/assets/play.png"
+           class="rec-list-play" @click="play(video.id)">
+    </el-row>
+    <p class="title">{{video.title}}</p>
+    <p style="position: relative;top: -30px;">
+      <span style="color: red;">{{video.uid}}</span>&nbsp;&nbsp;
+      <span style="font-size: 12px;">{{video.uptime}}</span>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,  // 显示、交互用的属性
+    }
+  },
+  props: {
+    video: Object,  // 用来接收父组件传递的数据
+  },
+  mounted() {
+    if (this.video) {
+      this.show = true
+    }
+  },
+  methods:{
+    play(id){
+      this.$router.push("/playPage/" + id)
+    }
+  }
+}
+</script>
+<style scoped>
+.item-box {
+  width: 100%;
+  position: relative;
+}
+.cover {
+  width: 100%;
+  height: auto;
+  border: 1px solid gainsboro;
+}
+.play-icon {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+.rec-list-play {
+  width: 100%;
+  height: 100%;
+}
+.rec-list-play:hover {
+  transform: scale(1.1);
+}
+.title {
+  margin-bottom: 15px;
+  font-weight: bold;
+  white-space: nowrap; /* 强制文本不换行 */
+  overflow: hidden; /* 隐藏溢出的内容 */
+  text-overflow: ellipsis; /* 使用省略号表示溢出的内容 */
+  display: inline-block; /* 将 p 标签设置为行内块元素，确保宽度生效 */
+}
+</style>
